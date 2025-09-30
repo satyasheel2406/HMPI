524825eff2c17cbec315ea354f41151641bf463d
satyasheel <satyasheelraman@gmail.com>
2025-09-30 14:56:37 +0530

    Your commit message

---
+++ server/server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+const cors = require('cors');
+
+// Load environment variables
+dotenv.config();
+
+// Connect to MongoDB
+connectDB();
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json()); // parse JSON bodies
+
+// Routes
+app.use('/api/auth', require('./routes/authRoutes'));
+app.use('/api/samples', require('./routes/sampleRoutes'));
+
+// Default route
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
+
+// Start server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
